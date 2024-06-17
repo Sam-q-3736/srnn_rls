68bd6bc422bfabd6ac162b34108d105b1426e1d4
Sam-q-3736 <sq3736@gmail.com>
2024-06-17 12:08:08 -0400

    genw_sparse created

---
+++ genw_sparse.py
@@ -0,0 +1,11 @@
+def genw_sparse(N, mean, std, p):
+    # this function returns a NxN matrix 
+    # each entry is non-zero with probability p
+    # entries are gaussian with mean m and standard deviation std
+    w = np.zeros((N, N)) # initialize NxN matrix of zeros
+    for i in range(N):
+        for j in range(N):
+            weight = sp.stats.norm.rvs(m, std)
+            is_zero = sp.stats.bernoulli.rvs(p)
+            w[i][j] = weight*is_zero
+    return w
