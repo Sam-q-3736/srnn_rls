955efcbd37567029bf246f242cfae18a9cbe0ea2
Sam-q-3736 <sq3736@gmail.com>
2024-06-24 11:45:53 -0400

    bugfix in genw_sparse

--- genw_sparse.py
+++ genw_sparse.py
@@ -4,5 +4,5 @@ import scipy as sp
 # returns a sparse NxN matrix with gaussian entries
 def genw_sparse(N, m, std, p):
     weight = sp.stats.norm.rvs(m, std, (N, N))
-    is_zero = np.random.choice([0, 1], (N, N), [1-p, p])    
+    is_zero = np.random.choice([0, 1], (N, N), p = [1-p, p])    
     return np.multiply(weight, is_zero)
