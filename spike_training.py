e6b8f5a8c56083504b99bda59e05e65881d44255
Sam-q-3736 <sq3736@gmail.com>
2024-06-24 14:36:39 -0400

    added init to base class

--- spike_training.py
+++ spike_training.py
@@ -5,20 +5,25 @@ import seaborn as sns
 
 class spike_training:
 
-    def genw_sparse(N, m, std, p):
+    def __init__(self):
+        raise NotImplementedError
+
+    def genw_sparse(self, N, m, std, p):
         weight = sp.stats.norm.rvs(m, std, (N, N))
         is_zero = np.random.choice([0, 1], (N, N), [1-p, p])    
         return np.multiply(weight, is_zero)
 
-    def train_network(neuron_params, time_params, train_params, W, stim, targets):
+    def train_network(self, neuron_params, time_params, train_params, W, stim, targets):
         raise NotImplementedError
 
-    def run_network(neuron_params, time_params, W, stim, run_time):
+    def run_network(self, neuron_params, time_params, W, stim, run_time):
         raise NotImplementedError
 
-    def plot_spk_rasts(spk_rast):
+    def plot_spk_rasts(self, spk_rast):
         spk_inds, spk_t = np.nonzero(spk_rast)
         spk_times = []
         for idx in np.unique(spk_inds):
             spk_times.append(spk_t[spk_inds == idx])
         plt.eventplot(spk_times[0:10]);
+
+    
