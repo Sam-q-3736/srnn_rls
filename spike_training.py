9264f16f188a0bb7f1016a2adbcaabe0f108627b
Sam-q-3736 <sq3736@gmail.com>
2024-06-24 16:52:22 -0400

    static method

--- spike_training.py
+++ spike_training.py
@@ -22,7 +22,7 @@ class spike_training:
     def run_network(self, neuron_params, time_params, W, stim, run_time):
         raise NotImplementedError
 
-    def plot_spk_rasts(self, spk_rast, inds):
+    def plot_spk_rasts(spk_rast, inds):
         spk_inds, spk_t = np.nonzero(spk_rast)
         spk_times = []
         for idx in np.unique(spk_inds):
