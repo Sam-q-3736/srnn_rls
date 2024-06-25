860e26db59b897951bf8e3af932efa25052c10c9
Sam-q-3736 <sq3736@gmail.com>
2024-06-25 17:58:08 -0400

    class of QIF spike training

--- QIF_training.py
+++ QIF_training.py
@@ -2,10 +2,9 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 import seaborn as sns
+from spike_training import *
 
-class QIF_training(spike_training):
-    
-    def create_default_params():
+def create_default_params():
         neuron_params = {
             'net_size': 200, # units in network
             'tau': 1, # ms, neuron decay constant
@@ -24,7 +23,7 @@ class QIF_training(spike_training):
         }
         connectivity_params = {
             'm': 0, # mean
-            'std': 1/np.sqrt(N), # standard deviation
+            'std': 1/np.sqrt(200), # standard deviation, 1/sqrt(netsize)
             'cp': 0.3, # connection probability
         }
         run_params = {
@@ -32,55 +31,89 @@ class QIF_training(spike_training):
         }
         return neuron_params, time_params, train_params, connectivity_params, run_params
 
+class QIF_training(spike_training):
+
     def __init__(self, neuron_params, time_params, train_params, connectivity_params, run_params):
-        self.neuron_params = neuron_params
-        self.time_params = time_params
-        self.train_params = train_params
-        self.connectivity_params = connectivity_params
-        self.run_params = run_params
-        self.W_init = genw_sparse(neuron_params['net_size'], connectivity_params['m'], connectivity_params['std'], connectivity_params['cp'])
+        self.W_init = self.genw_sparse(neuron_params['net_size'], connectivity_params['m'], connectivity_params['std'], connectivity_params['cp'])
         self.W_trained = np.copy(self.W_init)
 
-    def dtheta(theta, n_drive):
-        return 1/tau * (1 - np.cos(theta) + np.multiply(n_drive, (1 + np.cos(theta))))
-
-    def dr(r): 
-        return -1/tau_s * r 
-        # does not include addition of new spikes
-        
-    def rk4_step(theta, u, ext, r): 
-        k1 = dt * dtheta(theta, u + ext)
-        k2 = dt * dtheta(theta + k1/2, u + ext)
-        k3 = dt * dtheta(theta + k2/2, u + ext)
-        k4 = dt * dtheta(theta + k3, u + ext)
-        theta_next = theta + (k1 + 2*k2 + 2*k3 + k4)/6
-        r1 = dt * dr(r) 
-        r2 = dt * dr(r + r1/2)
-        r3 = dt * dr(r + r2/2)
-        r4 = dt * dr(r + r3)
-        r_next = r + (r1 + 2*r2 + 2*r3 + r4)/6
-        return theta_next, r_next
-
-    def train_QIF(self, neuron_params, time_params, train_params, W, stim, targets):
-            
         # unpack parameters
-        N, tau, tau_s, lam = neuron_params
-        T, stim_on, stim_off, dt = time_params
-        nloop, train_every = train_params
+        self.N = neuron_params['net_size']
+        self.tau = neuron_params['tau']
+        self.tau_s = neuron_params['tau_s']
+        self.lam = neuron_params['lam']
+
+        self.T = time_params['total_time']
+        self.stim_on = time_params['stim_on']
+        self.stim_off = time_params['stim_off']
+        self.dt = time_params['dt']
+        
+        self.nloop = train_params['training_loops']
+        self.train_every = train_params['train_every']
+
+        self.run_time = run_params['runtime']
 
         # initialize variables
-        theta = np.zeros(N) # phase of neurons
-        u = np.zeros(N) # synaptic drive
-        r = np.zeros(N) # filtered synaptic drive
-        spk_t = np.zeros(N) # tracks spikes
+        self.theta = np.zeros(self.N) # phase of neurons
+        self.u = np.zeros(self.N) # synaptic drive
+        self.r = np.zeros(self.N) # filtered synaptic drive
+        self.spk_t = np.zeros(self.N) # discrete spikes
+
+    def dtheta(self, theta_in, n_drive):
+        return 1/self.tau * (1 - np.cos(theta_in) + np.multiply(n_drive, (1 + np.cos(theta_in))))
+
+    def dr(self, r_in): 
+        return -1/self.tau_s * r_in
+        # does not include addition of new spikes
+        
+    def rk4_step(self, stim, t): 
+        if t < self.stim_off:
+            ext = stim[:, int(t/self.dt)]
+        else:
+            ext = np.zeros(self.N)
+        
+        # RK4 for theta
+        k1 = self.dt * self.dtheta(self.theta, self.u + ext);
+        k2 = self.dt * self.dtheta(self.theta + k1/2, self.u + ext);
+        k3 = self.dt * self.dtheta(self.theta + k2/2, self.u + ext);
+        k4 = self.dt * self.dtheta(self.theta + k3, self.u + ext);
+        theta_next = self.theta + (k1 + 2*k2 + 2*k3 + k4)/6;
+
+        # RK4 for filtered spike train (one step)
+        r1 = self.dt * self.dr(self.r) 
+        r2 = self.dt * self.dr(self.r + r1/2)
+        r3 = self.dt * self.dr(self.r + r2/2)
+        r4 = self.dt * self.dr(self.r + r3)
+        r_next = self.r + (r1 + 2*r2 + 2*r3 + r4)/6
+
+        # spike detection
+        idx1 = theta_next - self.theta > 0
+        idx2 = theta_next - self.theta > np.mod(np.pi - self.theta, 2*np.pi) # surely there's a better way to do this...
+        idx = np.multiply(idx1, idx2)
+        self.r[idx] += 1/self.tau_s # update spikes in r
+        self.spk_t[idx] += 1
+
+        # update variables
+        self.theta = np.mod(self.theta + (k1 + 2*k2 + 2*k3 + k4)/6, 2*np.pi)
+        self.r = self.r + (r1 + 2*r2 + 2*r3 + r4)/6
+        self.u = np.dot(self.W_trained, self.r)
+
+    def train_QIF(self, stim, targets):
+            
+        # initialize variables
+        # exclude to run from previous behavior
+        self.theta = np.zeros(self.N) # phase of neurons
+        self.u = np.zeros(self.N) # synaptic drive
+        self.r = np.zeros(self.N) # filtered synaptic drive
+        self.spk_t = np.zeros(self.N) # tracks spikes
         
         # initialize correlation matrices for each row
-        Pidx = np.zeros((N, N))
+        Pidx = np.zeros((self.N, self.N))
         Ps = []
-        for row in range(N):
-            Pidx[row] = (W[row] != 0)
+        for row in range(self.N):
+            Pidx[row] = (self.W_trained[row] != 0)
             tot = int(np.sum(Pidx[row]))
-            Ps.append(1/lam * np.identity(tot))
+            Ps.append(1/self.lam * np.identity(tot))
         Pidx = Pidx.astype(bool)
 
         # optional variables to track states
@@ -90,104 +123,84 @@ class QIF_training(spike_training):
         spk_rast = []
         
         # training loop
-        for i in range(nloop):
-            print(i)
+        for i in range(self.nloop):
+            print('training trial', i)
             t = 0.0
-            itr = 0
-            while t < T:
+            while t < self.T:
 
-                theta_next, r_next = rk4_step(theta, u, ext, r)
+                # calculate next timestep of variables and update
+                self.rk4_step(stim, t) 
 
-                # spike detection
-                idx1 = theta_next - theta > 0
-                idx2 = theta_next - theta > np.mod(np.pi - theta, 2*np.pi) # surely there's a better way to do this...
-                idx = np.multiply(idx1, idx2)
-                r[idx] += 1/tau_s # update spikes in r
-                spk_t[idx] += 1
+                # update timestep
+                t = t + self.dt
 
-                # update variables
-                theta = np.mod(theta_next, 2*np.pi)
-                r = r_next
-                u = np.dot(W, r)
-                t = t + dt
-                
                 # track variables 
-                spks.append(r)
-                sdrive.append(u)  
-                thetas.append(theta)
-                spk_rast.append(spk_t)
-                spk_t = np.zeros(N)
+                spks.append(self.r)
+                sdrive.append(self.u)  
+                thetas.append(self.theta)
+                spk_rast.append(self.spk_t)
+
+                # reset current spikes to 0
+                self.spk_t = np.zeros(self.N)
 
                 # train W matrix
-                if t > stim_off and t < int(T) and np.mod(int(t/dt), int(train_every/dt)) == 0: # only train after initial stimulus
-                    for row in range(N): # update each row of W by RLS
+                if t > self.stim_off and t < int(self.T) \
+                    and np.mod(int(t/self.dt), int(self.train_every/self.dt)) == 0: # only train after initial stimulus
+                    
+                    for row in range(self.N): # update each row of W by RLS
 
                         # update correlation matrix
-                        numer = np.outer(np.dot(Ps[row], r[Pidx[row]]), np.dot(Ps[row], r[Pidx[row]]))
-                        denom = 1 + np.dot(r[Pidx[row]], np.dot(Ps[row], r[Pidx[row]]))
+                        numer = np.outer(np.dot(Ps[row], self.r[Pidx[row]]), np.dot(Ps[row], self.r[Pidx[row]]))
+                        denom = 1 + np.dot(self.r[Pidx[row]], np.dot(Ps[row], self.r[Pidx[row]]))
                         Ps[row] = Ps[row] - numer / denom
 
                         # update error term
-                        err = targets[row][int(t/dt)] - np.dot(W[row][Pidx[row]], r[Pidx[row]]) # error is scalar
+                        err = targets[row][int(t/self.dt)] - \
+                            np.dot(self.W_trained[row][Pidx[row]], self.r[Pidx[row]]) # error is scalar
 
                         # update W
-                        W[row][Pidx[row]] = W[row][Pidx[row]] + err * np.dot(Ps[row], r[Pidx[row]])
+                        self.W_trained[row][Pidx[row]] \
+                            = self.W_trained[row][Pidx[row]] + err * np.dot(Ps[row], self.r[Pidx[row]])
             
         spks = np.transpose(spks)
         sdrive = np.transpose(sdrive)
         thetas = np.transpose(thetas)
         spk_rast = np.transpose(spk_rast)
-        return W, spks, sdrive, thetas, spk_rast
+        return spks, sdrive, thetas, spk_rast
 
-    def run_QIF(self, neuron_params, time_params, W, stim, run_time):
-
-        # unpack parameters
-        N, tau, tau_s, lam = neuron_params
-        T, stim_on, stim_off, dt = time_params
-        
+    def run_QIF(self, stim):
+            
         # initialize variables
-        theta = np.zeros(N) # phase of neurons
-        u = np.zeros(N) # synaptic drive
-        r = np.zeros(N) # filtered synaptic drive
-        spk_t = np.zeros(N) # discrete spikes
+        # exclude to run from previous behavior
+        self.theta = np.zeros(self.N) # phase of neurons
+        self.u = np.zeros(self.N) # synaptic drive
+        self.r = np.zeros(self.N) # filtered synaptic drive
+        self.spk_t = np.zeros(self.N) # tracks spikes
 
         # optional variables to track states
         spks = []
         sdrive = []
         thetas = []
         spk_rast = []
-
+        
         # training loop
         t = 0.0
-        itr = 0
-        while t < run_time:
-            
-            if t < stim_off:
-                ext = stim[:, int(t/dt)]
-            if t > stim_off:
-                ext = np.zeros(N)
+        while t < self.run_time:
 
-            theta_next, r_next = rk4_step(theta, u, ext, r)
+            # calculate next timestep of variables and update
+            self.rk4_step(stim, t) 
 
-            # spike detection
-            idx1 = theta_next - theta > 0
-            idx2 = theta_next - theta > np.mod(np.pi - theta, 2*np.pi) # surely there's a better way to do this...
-            idx = np.multiply(idx1, idx2)
-            r[idx] += 1/tau_s # update spikes in r
-            spk_t[idx] += 1
-            
-            # update variables
-            theta = np.mod(theta_next, 2*np.pi)
-            r = r_next
-            u = np.dot(W, r)
-            t = t + dt
-            
-            # track variables
-            spks.append(r)
-            sdrive.append(u)  
-            thetas.append(theta)
-            spk_rast.append(spk_t)
-            spk_t = np.zeros(N)
+            # update timestep
+            t = t + self.dt
+
+            # track variables 
+            spks.append(self.r)
+            sdrive.append(self.u)  
+            thetas.append(self.theta)
+            spk_rast.append(self.spk_t)
+
+            # reset current spikes to 0
+            self.spk_t = np.zeros(self.N)
 
         spks = np.transpose(spks)
         sdrive = np.transpose(sdrive)
